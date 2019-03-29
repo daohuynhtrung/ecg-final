1b1c72b6321196e546338a56658c5a7c8c739bf8
trung <daohuynhtrung43@gmail.com>
2019-03-29 17:00:04 +0700

    new model lstm

--- source/models/models2.py
+++ source/models/models2.py
@@ -14,20 +14,19 @@ def lstm_classifier(**kwargs):
     input_vector_size = kwargs.get('input_vector_size', 128)
     dense_size = kwargs.get('dense_size', 20)
     output = kwargs.get('label_size', 2)
-    timesteps = None
+    timesteps = 1
     xav_init = tf.contrib.layers.xavier_initializer()
     ##########
 
     model = Sequential()
-
-    model.add(LSTM(int(input_vector_size), input_shape=(timesteps, int(input_vector_size)), return_sequences=False))
-
-    model.add(Dense(dense_size, activation='softmax', kernel_initializer='glorot_normal'))
-
-    model.add(Dense(dense_size, activation='softmax', kernel_initializer='glorot_normal'))
-
-    model.add(Dense(output, activation='softmax'))
+    # model.add(LSTM(input_vector_size, input_shape=(timesteps, input_vector_size)))
+    model.add(LSTM(32 ,dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(20, activation='softmax', kernel_initializer='glorot_normal'))
+    model.add(Dropout(0.2))
+    model.add(Dense(20, activation='softmax', kernel_initializer='glorot_normal'))
+    model.add(Dropout(0.2))
+    model.add(Dense(2, activation='softmax'))
     
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     
     return model
