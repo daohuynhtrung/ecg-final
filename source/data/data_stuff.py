4da25ecfd7f620c96f6445dd4299561e7782f758
trung <daohuynhtrung43@gmail.com>
2019-05-11 13:28:48 +0700

    add multi classify

--- source/data/data_stuff.py
+++ source/data/data_stuff.py
@@ -18,7 +18,14 @@ mitbih_file = [100,108,113,117,122,201,207,212,222,231,101,105,109,114,118,123,2
 normal_files = [103,105,106,108,112,113,114,115,116,117,119,121,122,123,200,201,202,203,205,208,209,210,213,215,219,220,221,222,223,228,230,233,234]
 abnormal_files = [111,124,207,212,214,231,232]
 
-testing_set = [220, 233, 221]
+testing_set0 = [220, 233, 221]
+testing_set1 = [228, 215, 202]
+# test for multi classes
+testing_set2 = [116,215,201,207]
+
+# test weird normal
+testing_set3 = [105,108,114,117,203,213,219,222]
+
 
 physionet_path = '/home/trung/py/data/mitdb'
 csv_file_path = '/home/trung/py/data/mitbih_wfdb'
@@ -104,6 +111,42 @@ def rr_processing(file_name,k=0,offset=0):
         print(file_name)
         return [], []
 
+def rr_processing_multi_classes(file_name,k=0,offset=0):
+    try:
+        df = pd.read_csv(file_name, header=None, engine='python')
+
+        values = df.loc[:][0].tolist()
+        signal_type = df.loc[:][1].tolist()
+
+        c = [i for i in range(len(signal_type)) if signal_type[i] != '0']
+        # r_data = [values[c[i]:c[i + k]] for i in range(len(c) - k)]
+        labels = []
+        r_data = []
+        for i in range(len(c)-k-3):
+            data = np.array(values[ c[i] : (c[i+k]+offset)])
+            data = rr_normalize_time(data,360*k)
+            # data = rr_normalize_amplitude(data)
+            r_data.append(data)
+            if signal_type[c[i]]=='L' or signal_type[c[i+1]]=='L':
+                labels.append(1.)
+            elif signal_type[c[i]]=='R' or signal_type[c[i+1]]=='R':
+                labels.append(2.)
+            elif signal_type[c[i]]=='A' or signal_type[c[i+1]]=='A':
+                labels.append(3.)
+            elif signal_type[c[i]]=='V' or signal_type[c[i+1]]=='V':
+                labels.append(4.)
+            elif signal_type[c[i]]=='N':
+                labels.append(0.)
+            else:
+                labels.append(5.)
+
+        return r_data,labels
+
+    except Exception as e:
+        print(e)
+        print(file_name)
+        return [], []
+
 
 def data_prepare(files):
     
@@ -119,6 +162,22 @@ def data_prepare(files):
     # return np.array(all_data), np.array(all_label)
     
     return np.array(all_data), to_categorical(np.array(all_label))
+
+def data_prepare_multi_classes(files):
+    
+    all_data = []
+    all_label = []
+    for file in files:
+        datas, labels = rr_processing_multi_classes(file_name=file,k=1)
+        for data in datas:
+            all_data.append(data)
+        for label in labels:
+            all_label.append(label)
+    
+    # return np.array(all_data), np.array(all_label)
+    
+    return np.array(all_data), to_categorical(np.array(all_label))
+
 
 
 def data_pipeline_na_split(data_path):
@@ -147,6 +206,7 @@ def oversampling(data, labels, multime=2):
             n_data.append([data[i],labels[i]])
         else:
             a_data.append([data[i],labels[i]])
+    a_data = a_data*multime
     n_data = random.sample(n_data,len(n_data))
     a_data = random.sample(a_data,len(a_data))
     
@@ -157,6 +217,59 @@ def oversampling(data, labels, multime=2):
     # print(len(n_data))
     # print(len(a_data))
     data = a_data + n_data
+    shuffle_data = random.sample(data,len(data))
+    new_data = [ i[0] for i in shuffle_data ]
+    new_label = [ i[1] for i in shuffle_data ]
+    return np.array(new_data), np.array(new_label)
+
+def oversampling_multi_classes(data, labels):
+    N_data = []
+    L_data = []
+    R_data = []
+    A_data = []
+    V_data = []
+    O_data = []
+
+    for i in range(len(data)):
+        if np.array_equal(labels[i],[1,0,0,0,0,0]):
+            N_data.append([data[i],labels[i]])
+        elif np.array_equal(labels[i],[0,1,0,0,0,0]):
+            L_data.append([data[i],labels[i]])
+        elif np.array_equal(labels[i],[0,0,1,0,0,0]):
+            R_data.append([data[i],labels[i]])
+        elif np.array_equal(labels[i],[0,0,0,1,0,0]):
+            A_data.append([data[i],labels[i]])
+        elif np.array_equal(labels[i],[0,0,0,0,1,0]):
+            V_data.append([data[i],labels[i]])
+        else:
+            O_data.append([data[i],labels[i]])
+
+    # UpSampling
+    L_data = L_data*2
+    R_data = R_data*2
+    A_data = A_data*3
+    V_data = V_data
+    O_data = O_data
+
+    # Shuffle
+    N_data = random.sample(N_data,len(N_data))
+    L_data = random.sample(L_data,len(L_data))
+    R_data = random.sample(R_data,len(R_data))
+    A_data = random.sample(A_data,len(A_data))
+    V_data = random.sample(V_data,len(V_data))
+    O_data = random.sample(O_data,len(O_data))
+
+    #Down Sampling
+    N_data = N_data[0:40000]
+
+    # print(len(N_data))
+    # print(len(L_data))
+    # print(len(R_data))
+    # print(len(A_data))
+    # print(len(V_data))
+    # print(len(O_data))
+
+    data = N_data + L_data + R_data + A_data + V_data
     shuffle_data = random.sample(data,len(data))
     new_data = [ i[0] for i in shuffle_data ]
     new_label = [ i[1] for i in shuffle_data ]
@@ -190,6 +303,7 @@ def data_pipeline(data_path):
     data_files = [i for i in glob.glob(data_path+'/*.csv')]
 
     X_trainfiles, X_testfiles = train_test_split(data_files,test_size=0.2,shuffle=True)
+    print(X_testfiles)
 
     X_train, y_train = data_prepare(X_trainfiles)
     X_train, y_train = oversampling(X_train, y_train)
@@ -202,21 +316,40 @@ def data_pipeline(data_path):
     return X_train, y_train, X_test, y_test
 
 def data_pipeline_test_choosen(data_path):
+    testing_set = testing_set3
     training_set = list(set(mitbih_file)-set(testing_set))
     training_files = [data_path+'/'+str(training_file)+'.csv' for training_file in training_set]
     testing_files = [data_path+'/'+str(testing_file)+'.csv' for testing_file in testing_set]
     
     X_train, y_train = data_prepare(training_files)
-    # X_train, y_train = oversampling(X_train, y_train)
-
     X_test, y_test = data_prepare(testing_files)
 
+    X_train, y_train = oversampling(X_train, y_train)
 
     return X_train, y_train, X_test, y_test
 
+def data_pipeline_test_choosen_multi_classes(data_path):
+    testing_set = testing_set2
+    training_set = list(set(mitbih_file)-set(testing_set))
+    training_files = [data_path+'/'+str(training_file)+'.csv' for training_file in training_set]
+    testing_files = [data_path+'/'+str(testing_file)+'.csv' for testing_file in testing_set]
+    
+    X_train, y_train = data_prepare_multi_classes(training_files)
+    X_test, y_test = data_prepare_multi_classes(testing_files)
+
+    X_train, y_train = oversampling_multi_classes(X_train, y_train)
+
+    return X_train, y_train, X_test, y_test
+
+def data_testing(data_path):
+    testing_set = testing_set0
+    testing_files = [data_path+'/'+str(testing_file)+'.csv' for testing_file in testing_set]
+    X_test, y_test = data_prepare(testing_files)
+    return X_test, y_test
+
 def testing():
     X_train, y_train, X_test, y_test = data_pipeline('/home/trung/py/data/mitbih_wfdb_local') 
-    # return
+    return
     new_Xtrain = reduceDemensionPCA(X_train, 30)
 
     for no in range(10):
@@ -232,4 +365,4 @@ def testing():
         plt.show()
 
 # testing()
-# data_pipeline('/data/mitbih_wfdb')
+# data_pipeline_test_choosen('/data/mitbih_wfdb')
